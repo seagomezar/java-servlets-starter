f04f46283b7aee79dd2e9ef2fc736fb76dbdb225
Sebastian Gomez <seagomezar@eafit.edu.co>
2025-02-06 22:56:31 -0500

    First Commit

---
+++ src/main/com/example/HolaMundoServlet.java
@@ -0,0 +1,21 @@
+package com.example;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HolaMundoServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) 
+            throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter out = resp.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>Hola Mundo desde un Servlet!</h1>");
+        out.println("</body></html>");
+    }
+}
