8d9d399350947b93a8435b1d12ca17eb889b77cd
unknown <jclondonoo@eafit.edu.co>
2025-07-31 00:12:24 -0500

    Jean Londoño, profe pongame 5

---
+++ src/main/java/com/example/Jean2.java
@@ -0,0 +1,22 @@
+package com.example;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/jean2")
+public class Jean2 extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter out = resp.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>Soy Jean, tengo 19 años</h1>");
+        out.println("</body></html>");
+    }
+}
